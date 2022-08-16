875e2f246302cc82a4e9fc1ad1f3687690b23e26
Albert <zakhrabekyanalbert@gmail.com>
2022-08-16 06:59:36 +0000

    second coinflip commit

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -53,7 +53,7 @@ contract CoinFlip is Ownable {
         minDepositAmount = _minDepositAmount;
     }
 
-    function play( uint256 depAmount, uint256 choice ) payable external {
+    function play(uint256 depAmount, uint256 choice) payable external {
         require(choice == 0 || choice == 1, "CoinFlip: Incorrect choice");
         if(msg.value == 0){
             require(depAmount >= minDepositAmount && depAmount <= maxDepositAmount, "CoinFlip: Incorrect deposit amount");
@@ -61,7 +61,8 @@ contract CoinFlip is Ownable {
             require(token.allowance(msg.sender, address(this)) >= depAmount, "CoinFlip: Not enough allowance");
             token.transferFrom(msg.sender, address(this), depAmount);
             require(token.balanceOf(address(this)) > depAmount * coeff / 100, "CoinFlip: Not enough balance");
-        }else{
+        }
+        else{
             require(msg.value >= minDepositAmount && msg.value <= maxDepositAmount, "CoinFlip: Wrong deposit amount");
             depAmount = msg.value;
         }
@@ -79,6 +80,7 @@ contract CoinFlip is Ownable {
                 payable(msg.sender).transfer(game.prize);
             }
             games[totalGamesCount] = game;
+            
         } else {
             game.result = result;
             game.status = Status.LOSE;
@@ -89,45 +91,7 @@ contract CoinFlip is Ownable {
         
         totalGamesCount += 1;
 
-        emit GameFinished(
-            game.player,
-            game.depositAmount,
-            game.choice,
-            game.result,
-            game.prize,
-            game.status
-        );
-    }
-
-    function playWithEther( uint256 choice ) payable external {
-        require(choice == 0 || choice == 1, "CoinFlip: wrong coiche");
-        require(msg.value >= minDepositAmount && msg.value <= maxDepositAmount, "CoinFlip: Wrong deposit amount");
-        Game memory game = Game(msg.sender, msg.value, 1, 0, 0, Status.PENDING);
-        uint256 result = block.number % 2;
-        if(result == 1) {
-            game.result = result;
-            game.status = Status.WIN;
-            game.prize = msg.value * coeff / 100;
-            payable(msg.sender).transfer(game.prize);
-            games[totalGamesCount] = game;
-        } else {
-            game.result = result;
-            game.status = Status.LOSE;
-            game.prize = 0;
-            profit += game.depositAmount;
-            games[totalGamesCount] = game;
-        }
-        
-        totalGamesCount += 1;
-
-        emit GameFinished(
-            game.player,
-            game.depositAmount,
-            game.choice,
-            game.result,
-            game.prize,
-            game.status
-        );
+        emit GameFinished(game.player, game.depositAmount, game.choice, game.result, game.prize, game.status);
     }
 
     function withdraw(uint256 amount) external onlyOwner {
